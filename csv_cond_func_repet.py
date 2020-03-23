976fefc54fd261e4b064765b2af10eb7daa176bb
deodre <45459027+deodre@users.noreply.github.com>
2020-03-23 21:48:30 +0200

    csv_cond_funct_repet modified and works

--- csv_cond_func_repet.py
+++ csv_cond_func_repet.py
@@ -5,13 +5,13 @@ print("Salarii inainte de majorare: ", salarii)
 
 
 def majorare_salarii(lista_salarii):
-    for salariu in lista_salarii:
-        if salariu >= 2000 & salariu < 5000:
-            salariu += salariu * 0.05
-        elif salariu >= 5000 & salariu < 7000:
-            salariu += salariu * 0.03
+    for idx in range(len(lista_salarii)):
+        if lista_salarii[idx] >= 2000 & lista_salarii[idx] < 5000:
+            lista_salarii[idx] += lista_salarii[idx] * 0.05
+        elif lista_salarii[idx] >= 5000 & lista_salarii[idx] < 7000:
+            lista_salarii[idx] += lista_salarii[idx] * 0.03
         else:
-            salariu += salariu * 0.02
+            lista_salarii[idx] += lista_salarii[idx] * 0.02
     return lista_salarii
 
 
