47b55c119786b68c3642306249514550ca51d858
Ioana Berdei <andreea.ioanaa98@gmail.com>
2020-03-25 15:29:09 +0200

    loc iloc modificare pandas

--- csv_cond_func_repet.py
+++ csv_cond_func_repet.py
@@ -1,20 +1,29 @@
 import pandas as pd
 date = pd.read_csv("angajati.csv")
-salarii = date["Salariu"].tolist()
-print("Salarii inainte de majorare: ", salarii)
+# salarii = date["Salariu"].tolist()
+# print("Salarii inainte de majorare: ", salarii)
+#
+#
+# def majorare_salarii(lista_salarii):
+#     for idx in range(len(lista_salarii)):
+#         if lista_salarii[idx] >= 2000 & lista_salarii[idx] < 5000:
+#             lista_salarii[idx] += lista_salarii[idx] * 0.05
+#         elif lista_salarii[idx] >= 5000 & lista_salarii[idx] < 7000:
+#             lista_salarii[idx] += lista_salarii[idx] * 0.03
+#         else:
+#             lista_salarii[idx] += lista_salarii[idx] * 0.02
+#     return lista_salarii
+#
+#
+# salarii = majorare_salarii(salarii)
+# print("Salarii dupa majorare: ", salarii)
 
+# ACCESARE LOC SI ILOC
+# print(date.iloc[0:3, 0:3])
+# print(date.loc[0:2, ['Nume', 'Functie', 'Salariu']])
 
-def majorare_salarii(lista_salarii):
-    for idx in range(len(lista_salarii)):
-        if lista_salarii[idx] >= 2000 & lista_salarii[idx] < 5000:
-            lista_salarii[idx] += lista_salarii[idx] * 0.05
-        elif lista_salarii[idx] >= 5000 & lista_salarii[idx] < 7000:
-            lista_salarii[idx] += lista_salarii[idx] * 0.03
-        else:
-            lista_salarii[idx] += lista_salarii[idx] * 0.02
-    return lista_salarii
-
-
-salarii = majorare_salarii(salarii)
-print("Salarii dupa majorare: ", salarii)
-
+# MODIFICARE DATE
+date.loc[7, 'Functie'] = 'Front End Developer'
+date.loc[7, 'Salariu'] = 8000
+print("Dupa actualizarea functiei, informatiile despre angajat sunt:")
+print(date.loc[7])
